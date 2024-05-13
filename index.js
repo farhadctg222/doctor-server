973e77274339cd03853340a0888270202baa1265
FARHADCHATTOGRAM <taxctg21@gmail.com>
2024-05-13 20:29:19 +0600

    d

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const cookieParser = require( 'cookie-parser')
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://doctor:MI6UlA1ARPCILMyX@cluster0.cgsbnjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASS}@cluster0.cgsbnjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 const port = process.env.port || 5000;
  
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: [
+    "http://localhost:5000",
+    "https://doctor-f8ea2.web.app",
+    "https://doctor-f8ea2.firebaseapp.com"
+  ],
   credentials: true
 }))
 app.use(cookieParser())
@@ -36,18 +40,35 @@ const client = new MongoClient(uri, {
 });
 
 const loger = (req,res,next)=>{
-  console.log('cald',req.host,req.post)
-  console.log(req.body.cookie.token)
+  console.log('cald',req.method,req.url)
+ 
   next()
 }
 const verify = (req,res,nex)=>{
+  const token = req?.cookies?.token;
+  console.log('token midle ware',token)
+  if(!token){
+    return res.status(401).send({message: 'unauthoried access'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN,(error,decode)=>{
+    if(error){
+      return res.status(401).send({message: 'unathorized access'})
+    }
+    req.user = decode
+  })
+  nex()
 
+}
+const cookieOption = {
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV ==='Production'? 'none': 'strict',
+  secure: process.env.NODE_ENV ==='Production'? true: 'strict',
 }
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
       const db = client.db('Doctor').collection('service')
       const order = client.db('ServiceData').collection('Order')
@@ -76,8 +97,12 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/book',loger,async (req,res)=>{
+    app.get('/book',loger,verify,async (req,res)=>{
         console.log(req.query.email)
+        console.log('token owner', req.user)
+        if(req.user.email!==req.query.email){
+        return  res.status(403).send({message:'forbiden access'})
+        }
         let query = {}
         if(req.query?.email){
           query = { email : req.query.email }
@@ -123,7 +148,7 @@ async function run() {
 
 
   
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
